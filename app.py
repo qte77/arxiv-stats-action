5c951c115adfb3b992b2b7021fe4c11ee9084ab1
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-08 11:56:42 +0200

    Transfered reponse to .utils/get_api_response()

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import .utils
 import csv
 from os import getenv, makedirs
 from os.path import exists, dirname
-import urllib.request
 
 CSV_FILE = getenv("CSV_FILE")
 TOPICS = getenv("TOPICS")
@@ -16,6 +15,12 @@ TOPICS_REPL_STR = "#TOPICS#"
 MAXRES_REPL_STR = "#MAXRES#"
 STARTRES_REPL_STR = "#STARTRES#"
 
+# https://github.com/karpathy/arxiv-sanity-lite/blob/d7a303b410b0246fbd19087e37f1885f7ca8a9dc/aslite/arxiv.py#L15
+# https://info.arxiv.org/help/api/user-manual.html
+# sortOrder=descending
+add_url = ADD_URL.replace(TOPICS_REPL_STR, TOPICS).replace(MAXRES_REPL_STR, str(MAX_RESULTS_PER_QUERY))
+search_query = BASE_URL + add_url
+
 if not exists(CSV_FILE):
   # folder needs to exist before open() context
   makedirs(dirname(CSV_FILE), exist_ok=True)
@@ -24,18 +29,9 @@ if not exists(CSV_FILE):
   with open(CSV_FILE, 'w+', newline='', encoding='UTF8') as f:
     csv.writer(f).writerow(header)
 
-# https://github.com/karpathy/arxiv-sanity-lite/blob/d7a303b410b0246fbd19087e37f1885f7ca8a9dc/aslite/arxiv.py#L15
-# https://info.arxiv.org/help/api/user-manual.html
-# sortOrder=descending
-add_url = ADD_URL.replace(TOPICS_REPL_STR, TOPICS).replace(MAXRES_REPL_STR, str(MAX_RESULTS_PER_QUERY))
-search_query = BASE_URL + add_url
-
 for k in range(START_RESULT, START_RESULT + END_RESULT, MAX_RESULTS_PER_QUERY):
   search_query_k = search_query.replace(STARTRES_REPL_STR, str(k))
-  with urllib.request.urlopen(search_query_k) as url:
-      response = url.read()
-  if url.status != 200:
-      print(f"arxiv did not return status 200 response")
+  response = get_api_response(search_query_k)
   out = get_parsed_output(response)
   with open(CSV_FILE, 'a+', newline='', encoding='UTF8') as f:
     writer = csv.writer(f)
