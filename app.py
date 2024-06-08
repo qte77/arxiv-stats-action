abeb46c31e91b557fca0e463eb62e3e0deb8a2ce
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-08 12:21:20 +0200

    Changed csv.writer call

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from utils import get_api_response, get_parsed_output
-from csv import writer
+import csv
 from os import getenv, makedirs
 from os.path import exists, dirname
 
@@ -15,26 +15,26 @@ TOPICS_REPL_STR = "#TOPICS#"
 MAXRES_REPL_STR = "#MAXRES#"
 STARTRES_REPL_STR = "#STARTRES#"
 
+HEADER = ["published", "updated", "id", "version", "title"]
+
 # https://github.com/karpathy/arxiv-sanity-lite/blob/d7a303b410b0246fbd19087e37f1885f7ca8a9dc/aslite/arxiv.py#L15
 # https://info.arxiv.org/help/api/user-manual.html
 # sortOrder=descending
-add_url = ADD_URL.replace(TOPICS_REPL_STR, TOPICS).replace(MAXRES_REPL_STR, str(MAX_RESULTS_PER_QUERY))
-search_query = BASE_URL + add_url
+add_url_dyn = ADD_URL.replace(TOPICS_REPL_STR, TOPICS).replace(MAXRES_REPL_STR, str(MAX_RESULTS_PER_QUERY))
+search_query = BASE_URL + add_url_dyn
 
 if not exists(CSV_FILE):
   # folder needs to exist before open() context
   makedirs(dirname(CSV_FILE), exist_ok=True)
-  header = ["published", "updated", "id", "version", "title"]
-  print(f"{header}")
   with open(CSV_FILE, 'w+', newline='', encoding='UTF8') as f:
-    writer = writer(f)
-    writer.writerow(header)
+    writer = csv.writer(f)
+    writer.writerow(HEADER)
 
 for k in range(START_RESULT, START_RESULT + END_RESULT, MAX_RESULTS_PER_QUERY):
   search_query_k = search_query.replace(STARTRES_REPL_STR, str(k))
   response = get_api_response(search_query_k)
   out = get_parsed_output(response)
   with open(CSV_FILE, 'a+', newline='', encoding='UTF8') as f:
-    writer = writer(f)
+    writer = csv.writer(f)
     for o in out:
       writer.writerow(o)
