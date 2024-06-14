57e2fcb7cf49b65911743b3cc6a639ac5570d441
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-14 23:43:08 +0200

    Added test datetime weekday

--- app/app.py
+++ app/app.py
@@ -2,13 +2,14 @@ import csv
 from os import getenv, makedirs
 from os.path import exists, dirname
 from utils import get_api_response, get_parsed_output
+from datetime import date
 
 OUT_FILE = getenv("OUT_FILE", 'data/data.csv')
 TOPICS = getenv("TOPICS", 'cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.AI+OR+cat:cs.NE+OR+cat:cs.RO')
 BASE_URL = getenv("BASE_URL", 'http://export.arxiv.org/api/query?')
 ADD_URL = getenv("ADD_URL", 'search_query=#TOPICS#&start=#STARTRES#&max_results=#MAXRES#&sortBy=submittedDate')
 START_RESULT = getenv("START_RESULT", 0)
-END_RESULT = getenv("START_RESULT", 199)
+END_RESULT = getenv("START_RESULT", 19)
 MAX_RESULTS_PER_QUERY = getenv("MAX_RESULTS_PER_QUERY", 100)
 
 TOPICS_REPL_STR = "#TOPICS#"
@@ -37,4 +38,5 @@ for k in range(START_RESULT, START_RESULT + END_RESULT, MAX_RESULTS_PER_QUERY):
   with open(OUT_FILE, 'a+', newline='', encoding='UTF8') as f:
     writer = csv.writer(f)
     for o in out:
-      writer.writerow(o)
+      print(f"{date(o["published"]).isocalendar().week=}")
+      # writer.writerow(o)
