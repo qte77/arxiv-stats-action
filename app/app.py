32a6a56a8222c54d75d695ae0cd3b6554dd2ea01
qte77 <93844790+qte77@users.noreply.github.com>
2024-06-15 12:44:52 +0200

    Refactored write file to loop through out dict instead list

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from os import getenv, makedirs
 from os.path import exists, dirname
 from utils import get_api_response, get_parsed_output
 
-OUT_FILE = getenv("OUT_FILE", 'data/data.csv')
+OUT_DIR = getenv("OUT_DIR", './data')
 TOPICS = getenv("TOPICS", 'cat:cs.CV+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.AI+OR+cat:cs.NE+OR+cat:cs.RO')
 BASE_URL = getenv("BASE_URL", 'http://export.arxiv.org/api/query?')
 ADD_URL = getenv("ADD_URL", 'search_query=#TOPICS#&start=#STARTRES#&max_results=#MAXRES#&sortBy=submittedDate')
@@ -15,7 +15,7 @@ TOPICS_REPL_STR = "#TOPICS#"
 MAXRES_REPL_STR = "#MAXRES#"
 STARTRES_REPL_STR = "#STARTRES#"
 
-HEADER = ["published", "weekday (mon=0)", "updated", "id", "version", "title"]
+HEADER = ["Published", "Weekday (Monday==0)", "Updated", "ID", "Version", "Title"]
 
 # https://github.com/karpathy/arxiv-sanity-lite/blob/d7a303b410b0246fbd19087e37f1885f7ca8a9dc/aslite/arxiv.py#L15
 # https://info.arxiv.org/help/api/user-manual.html
@@ -23,21 +23,22 @@ HEADER = ["published", "weekday (mon=0)", "updated", "id", "version", "title"]
 add_url_dyn = ADD_URL.replace(TOPICS_REPL_STR, TOPICS).replace(MAXRES_REPL_STR, str(MAX_RESULTS_PER_QUERY))
 search_query = BASE_URL + add_url_dyn
 
-if not exists(OUT_FILE):
-  # folder needs to exist before open() context
-  makedirs(dirname(OUT_FILE), exist_ok=True)
-  with open(OUT_FILE, 'w+', newline='', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    writer.writerow(HEADER)
-
 for k in range(START_RESULT, START_RESULT + END_RESULT, MAX_RESULTS_PER_QUERY):
   search_query_k = search_query.replace(STARTRES_REPL_STR, str(k))
   response = get_api_response(search_query_k)
   out = get_parsed_output(response)
-  with open(OUT_FILE, 'a+', newline='', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    print(f"start output, {len(out)=}")
-    for o in out:
-      print(f"{len(o)=}, {o=}")
-      print(o[2])
-      # writer.writerow(o)
+  for k in out.keys():
+    out_file = f"{OUT_DIR}/{k}.csv"
+    if not exists(out_file):
+      # folder needs to exist before open() context
+      makedirs(dirname(out_file), exist_ok=True)
+      with open(out_file, 'w+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(HEADER)
+    with open(out_file, 'a+', newline='', encoding='UTF8') as f:
+      writer = csv.writer(f)
+      print(f"start output, {len(out[k])=}")
+      for o in out[k]:
+        print(f"{len(o)=}, {o=}")
+        print(o[2])
+        # writer.writerow(o)
